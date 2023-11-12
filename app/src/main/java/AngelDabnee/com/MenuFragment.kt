e315168fb48a5aad89cb8b130e0dac7befde6984
AngelDabnee <a22311046@uthermosillo.edu.mx>
2023-11-12 00:30:19 -0700

    terminamos primera parte de la navegacion
    Signed-off-by: AngelDabnee <a22311046@uthermosillo.edu.mx>

---
+++ app/src/main/java/AngelDabnee/com/MenuFragment.kt
@@ -0,0 +1,34 @@
+package AngelDabnee.com
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+class MenuFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_menu, container, false)
+        val btnProduct = view.findViewById<ImageView>(R.id.imgProduct)
+        val btnSuppler = view.findViewById<ImageView>(R.id.imgSuppliers)
+        val btnUser = view.findViewById<ImageView>(R.id.imgUsers)
+        val btnExit = view.findViewById<ImageView>(R.id.imgExit)
+
+        btnProduct.setOnClickListener{
+            val navController : NavController = Navigation.findNavController(view)
+            navController.navigate(R.id.action_menuFragment_to_productFragment)
+        }
+
+
+
+        return view
+    }
+}
