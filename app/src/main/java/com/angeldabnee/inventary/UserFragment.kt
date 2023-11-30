3d4cd6fce0d9a5d0a2542c4af6df52fa0a62adab
AngelDabnee <a22311046@uthermosillo.edu.mx>
2023-11-30 16:10:42 -0700

    cambiamos url de la imagen de los usuarios
    Signed-off-by: AngelDabnee <a22311046@uthermosillo.edu.mx>

--- app/src/main/java/com/angeldabnee/inventary/UserFragment.kt
+++ app/src/main/java/com/angeldabnee/inventary/UserFragment.kt
@@ -35,8 +35,8 @@ class UserFragment : Fragment() {
     }
     fun initData(){
         users = listOf(
-            UserData(1,"Angel Dabnee","6622557788","dabnee@gmail.com","AngelDabnee","https://scontent.fhmo1-2.fna.fbcdn.net/v/t39.30808-6/399722136_1417271112535651_8075701572657257127_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Qkk5BT1BDsUAX_3fJdT&_nc_ht=scontent.fhmo1-2.fna&oh=00_AfAHyIa9V71fZ8JcnnCju547pMzOtSGBinpIzAxIaIBttw&oe=6568154F"),
-            UserData(2,"Glenda Morales","6655889977","glenda@gmail.com","GlendaMorales","https://scontent.fhmo1-2.fna.fbcdn.net/v/t39.30808-1/349012138_152303231037038_8434918744964554763_n.jpg?stp=dst-jpg_p320x320&_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=UhN59n68m64AX8GE4aI&_nc_ht=scontent.fhmo1-2.fna&oh=00_AfCUsqsk8ikXMU82eZBkG1qRmfaQVgX6qH6y71mEO46_HQ&oe=6568B989"),
+            UserData(1,"Angel Dabnee","6622557788","dabnee@gmail.com","AngelDabnee","https://w0.peakpx.com/wallpaper/438/947/HD-wallpaper-el-rey-spiderman-chido-clasico.jpg"),
+            UserData(2,"Glenda Morales","6655889977","glenda@gmail.com","GlendaMorales","https://i.etsystatic.com/41932313/r/il/cb3152/5272343156/il_fullxfull.5272343156_q52x.jpg"),
             UserData(3,"Luis Acuña","6699887744","acuña@gmail.com","Luis Acuña","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuPQ1XK3TdLJu4fBDqtGXdcioOo8hVk4jWOw&usqp=CAU")
         )
     }
