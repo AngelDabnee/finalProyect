1c63c1bf1408004b11e0a08da6d44c407b362570
AngelDabnee <a22311046@uthermosillo.edu.mx>
2023-12-02 12:01:47 -0700

    Creamos la toast y guardamos los nuevos productos en una variable para mostrarlos en la lista
    Signed-off-by: AngelDabnee <a22311046@uthermosillo.edu.mx>

--- app/src/main/java/com/angeldabnee/inventary/ProductFragment.kt
+++ app/src/main/java/com/angeldabnee/inventary/ProductFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialog
@@ -23,12 +26,49 @@ class ProductFragment : Fragment() {
         val view =  inflater.inflate(R.layout.fragment_product, container, false)
         var recyclerView = view.findViewById<RecyclerView>(R.id.productsRecycle)
 
+
+        var bsd_new_id : Int =0
+        var bsd_new_name:String
+        var bsd_new_price:Double
+        var bsd_new_quantity:Int
+        var bsd_new_unit:String
+        var bsd_new_image:String
+
         val fab = view.findViewById<FloatingActionButton>(R.id.fab_new_product)
         fab.setOnClickListener{
             val bottomSheetDialog = BottomSheetDialog(view.context)
             val parentView : View = layoutInflater.inflate(R.layout.bsd_new_product,null)
             bottomSheetDialog.setContentView(parentView)
             bottomSheetDialog.show()
+
+            val newId = parentView.findViewById<EditText>(R.id.bsd_product_id)
+            val newName = parentView.findViewById<EditText>(R.id.bsd_product_name)
+            val newPrice = parentView.findViewById<EditText>(R.id.bsd_product_price)
+            val newQuantity = parentView.findViewById<EditText>(R.id.bsd_product_quantity)
+            val newUnit = parentView.findViewById<EditText>(R.id.bsd_product_unit)
+            val newImage = parentView.findViewById<EditText>(R.id.bsd_product_image)
+
+            val btnSubmit = parentView.findViewById<Button>(R.id.bsd_product_submit)
+
+
+            btnSubmit.setOnClickListener{
+                bsd_new_id = newId.text.toString().toInt()
+                bsd_new_name = newName.text.toString()
+                bsd_new_price = newPrice.text.toString().toDouble()
+                bsd_new_quantity = newQuantity.text.toString().toInt()
+                bsd_new_unit = newUnit.text.toString()
+                bsd_new_image = newImage.text.toString()
+                val toastMessage = "$bsd_new_id $bsd_new_name $bsd_new_price $bsd_new_quantity $bsd_new_unit $bsd_new_image"
+                val newProductAdd = ProductStock(bsd_new_id,bsd_new_name,bsd_new_price,bsd_new_quantity,bsd_new_unit,bsd_new_image)
+
+                products.toMutableList().add(newProductAdd)
+
+                recyclerView.adapter?.notifyDataSetChanged()
+
+                Toast.makeText(context,toastMessage,Toast.LENGTH_SHORT).show()
+                bottomSheetDialog.dismiss()
+            }
+
         }
         initData()
         var gridLayoutManager = GridLayoutManager(container?.context,2)
@@ -36,9 +76,6 @@ class ProductFragment : Fragment() {
         var adapter =  ProductAdapter(products)
         recyclerView?.layoutManager = gridLayoutManager
         recyclerView?.adapter = adapter
-
-
-
 
 
         return view
@@ -54,8 +91,7 @@ class ProductFragment : Fragment() {
             ProductStock(7,"Xbox Series S",5368.98,6,"Piezas","https://i5.walmartimages.com.mx/mg/gm/1p/images/product-images/img_large/00088984265135-2l.jpg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"),
             ProductStock(8,"Xbox Series X",50045.65,9,"Piezas","https://i5.walmartimages.com.mx/mg/gm/1p/images/product-images/img_large/00088984265135-2l.jpg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"),
             ProductStock(9,"Control Xbox One Blanco",699.69,5,"Piezas","https://m.media-amazon.com/images/I/71WX6wVepIL._AC_UF1000,1000_QL80_.jpg"),
-            ProductStock(10,"Diadema Xbox LVL50",599.69,5,"Piezas","https://resources.claroshop.com/imagenes-sanborns-ii/1200/708056064549.jpg")
+            ProductStock(10,"Diadema Xbox LVL50",599.69,5,"Piezas","https://resources.claroshop.com/imagenes-sanborns-ii/1200/708056064549.jpg"),
         )
     }
-
 }
